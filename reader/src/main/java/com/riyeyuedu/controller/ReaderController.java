0d20501a13a9ac8bd9c085e75525bf13148910ad
Neal-yangyi <36299231+Neal-yangyi@users.noreply.github.com>
2020-02-26 17:02:13 +0800

    Update ReaderController.java
    隐藏敏感信息

--- reader/src/main/java/com/riyeyuedu/controller/ReaderController.java
+++ reader/src/main/java/com/riyeyuedu/controller/ReaderController.java
@@ -111,8 +111,8 @@ public class ReaderController {
             // endpoint以杭州为例，其它region请按实际情况填写
             String endpoint = "http://oss-cn-beijing.aliyuncs.com";
             // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建
-            String accessKeyId = "LTAICBaKv7FrgzZf";
-            String accessKeySecret = "0CWhs6GjKdOqhGWNxZod3jM1npFdE7";
+            String accessKeyId = "******";
+            String accessKeySecret = "******";
             // 创建OSSClient实例
             OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
             // 上传文件流
